56f48011d9cdd58b6f68ec7ce483488ad34fdecc
lindsay <lindsaychognon@gmail.com>
2023-04-27 16:04:31 +0200

    Create Client Controller

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Clients;
+use Illuminate\Http\Request;
+
+class ClientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('clients.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $clients = new Clients;
+        $clients->societe = $request->societe;
+        $clients->nom = $request->nom;
+        $clients->prenom = $request->prenom;
+
+        $clients->save();
+
+        return redirect()->route('noms_de_domaine.index')->with('message', 'Le client a bien été ajouté, il est maintenant disponible dans la liste des clients lors de l\'ajout d\'un nom de domaine.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(clients $clients)
+    {
+        //
+    }
+}
