7e50ea1c60172e8aad972f57238970a96a88b111
lindsay <lindsaychognon@gmail.com>
2023-04-27 16:17:00 +0200

    Seeder table noms_de_domaine

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // seed clients table
         for ($i = 1; $i < 11; $i++) {
             DB::table('clients')->insert([
                 'societe' => 'Société ' . $i,
@@ -21,6 +22,17 @@ class DatabaseSeeder extends Seeder
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s")
                 ]);
+        }
+
+        // seed noms_de_domaine table
+        for ($i = 1; $i < 11; $i++) {
+            DB::table('noms_de_domaines')->insert([
+                'nom_domaine' => 'Nom de domaine' . $i,
+                'cout_annuel' =>  rand(10, 100),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+                'client_id' => rand(1, 10)
+            ]);
         }
     }
 }
