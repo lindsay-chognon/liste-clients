4301044b2036e104c41665b2acabee4e8953feca
lindsay <lindsaychognon@gmail.com>
2023-04-27 13:59:59 +0200

    Database migrations

---
+++ database/migrations/2023_04_27_111923_create_noms_de_domaines_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('noms_de_domaines', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_domaine', 255)->unique();
+            $table->float('cout_annuel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('noms_de_domaines');
+    }
+};
