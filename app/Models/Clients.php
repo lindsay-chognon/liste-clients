2a6c391d3c14e939f5c0be0e3c83f94be030b958
lindsay <lindsaychognon@gmail.com>
2023-04-27 17:10:45 +0200

    Ajout du one to many dans les models Clients et Noms_de_domaine

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class clients extends Model
+class Clients extends Model
 {
     use HasFactory;
+
+    /*
+     * Récupérer les noms de domaine d'un client
+     */
+
+    public function noms_de_domaines(): HasMany {
+        return $this->hasMany(Noms_de_domaine::class);
+    }
+
 }
--- app/Models/Noms_de_domaine.php
+++ app/Models/Noms_de_domaine.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Noms_de_domaine extends Model
 {
     use HasFactory;
+
+    /*
+     * Récupérer le client d'un nom de domaine
+     */
+
+    public function client(): BelongsTo {
+        return $this->belongsTo(Clients::class);
+    }
+
 }
